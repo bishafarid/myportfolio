3713dcec37c967b3556de2f0294494a9e929f83b
bishafarid <bishasiddiqui228@gmail.com>
2025-01-10 00:01:27 +0500

    Changes

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    alert("Welcome to Bisha Fareed's Portfolio!");
+};
+
+function handleFormSubmit(event) {
+    event.preventDefault(); 
+    alert("Thank you for your message! We will get back to you soon.");
+}
